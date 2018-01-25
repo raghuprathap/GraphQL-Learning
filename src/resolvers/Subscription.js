5cbc1de9d3e46d8021f7e7ccd1e6b2d6887f0bde
raghu.prathap <raghu.prathap@stackroute.in>
2018-01-25 09:22:33 +0530

    baby steps towards GraphQL

---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,24 @@
+const newLink = {
+    subscribe: (parent, args, context, info) => {
+        return context.db.subscription.link(
+            {
+                where: {mutation_in: ['CREATED']}
+            },
+            info,
+        )
+    },
+}
+
+const newVote = {
+    subscribe: (parent, args, context, info) => {
+        return context.db.subscription.vote(
+            {where: {mutation_in: ['CREATED']}},
+            info,
+        )
+    },
+}
+
+module.exports = {
+    newLink,
+    newVote
+}
